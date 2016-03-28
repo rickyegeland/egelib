7d7b16914db19e3e0ecf5eb292ec0d46f094fdc5
Ricky Egeland <ricky.egeland@gmail.com>
2016-03-28 11:19:30 -0600

    Add chi2_gof(): chi^2 goodness of fit statistic

--- egelib/stats.py
+++ egelib/stats.py
@@ -3,3 +3,10 @@ def printstats(x, label, precision=3):
     fmt = "N=%%i min=%%0.%(p)if max=%%0.%(p)if median=%%0.%(p)if mean=%%0.%(p)if std=%%0.%(p)if" % dict(p=precision)
     print label, fmt % \
     (x.size, x.min(), x.max(), np.median(x), x.mean(), x.std())
+
+def chi2_gof(x, model, e_x, n_params=0):
+    """Chi 2 goodness-of-fit test"""
+    chi2 = ((x - model)**2 / e_x**2).sum()
+    n_obs = x.size
+    dof = n_obs - n_params - 1
+    return chi2, chi2/dof
