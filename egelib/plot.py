581ee55a766b7ae4a4279a9bdef740fe3a92f06b
Ricky Egeland <ricky.egeland@protonmail.com>
2019-02-07 16:32:59 -0700

    Add plot.set_ticks(), for setting plot ticks/markers

--- egelib/plot.py
+++ egelib/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 
 def square_range(x, y=None, identity=True, margin=0.1):
     """Make the x range equal to the y range and plot an identity line """
@@ -15,3 +16,17 @@ def square_range(x, y=None, identity=True, margin=0.1):
         linex = np.linspace(min, max, 3)
         liney = linex
         plt.plot(linex, liney, 'k-', label='y = x')
+
+def set_ticks(majmult, minmult, majlen=10, minlen=5, size=18, axis='y'):
+    if axis == 'y':
+        ax = plt.gca().yaxis
+    elif axis == 'x':
+        ax = plt.gca().xaxis
+    elif isinstance(axis, mpl.axis.Axis):
+        ax = axis
+    else:
+        raise Exception('axis not specified correctly')
+    ax.set_major_locator(mpl.ticker.MultipleLocator(majmult))
+    ax.set_minor_locator(mpl.ticker.MultipleLocator(minmult))
+    ax.set_tick_params(which='major', direction='in', labelsize=size, length=majlen)
+    ax.set_tick_params(which='minor', direction='in', length=minlen)
